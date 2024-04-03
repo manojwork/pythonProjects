98e65c9c662b2d585cfae89d40f9285011de20d8
manojwork <142772975+manojwork@users.noreply.github.com>
2024-04-03 14:31:12 +0530

    P32_MondayMotivation.py

--- Email/P32_MondayMotivation.py
+++ Email/P32_MondayMotivation.py
@@ -12,6 +12,6 @@ f.close()
 if day==3:
     with sm.SMTP("smtp.gmail.com",port=587) as connect :
         connect.starttls()
-        connect.login(user=my_mail,password="bvydresbbflrglwb")
+        connect.login(user=my_mail,password="bvesbbflrglwb")
         connect.sendmail(from_addr=my_mail,to_addrs=resever,msg=f"subject:MONDAY MOTIVATION\n\n {quote} ")
         
